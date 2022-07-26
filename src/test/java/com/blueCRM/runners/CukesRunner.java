b3d8e9fa81f237a0868c8fae3214f4d223259071
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-07-26 16:30:30 +0300

    some important issue fixed

--- src/test/java/com/blueCRM/runners/CukesRunner.java
+++ src/test/java/com/blueCRM/runners/CukesRunner.java
@@ -7,13 +7,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html",
+                "html:target/cucumber/report.html",
                 "rerun:target/rerun.txt",
                 "json:target/cucumber.json",
         },
         features = "src/test/resources/features",
         glue =    "com/blueCRM/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = ""
 
 )
