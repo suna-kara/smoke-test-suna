b4a4aaca237048c866d5868f3de3dcdc0958edde
Iremgunes <aviremdanacioglu@gmail.com>
2022-07-27 21:48:19 +0300

    irem's feature pages and step definitions

--- src/test/java/com/blueCRM/runners/CukesRunner.java
+++ src/test/java/com/blueCRM/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue =    "com/blueCRM/step_definitions",
         dryRun = false,
-        tags = "@wip and @irem"
+        tags = ""
 
 )
 
