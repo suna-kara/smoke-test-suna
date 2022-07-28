dfe03564ac3c686474543c92ae692781c77719f8
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-07-28 12:31:04 +0300

    some important issue fixed

--- src/test/java/com/blueCRM/pages/Pages_mehmet.java
+++ src/test/java/com/blueCRM/pages/Pages_mehmet.java
@@ -1,4 +1,34 @@
 package com.blueCRM.pages;
 
+import com.blueCRM.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class Pages_mehmet {
+
+    public Pages_mehmet() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//input[@placeholder='Login']")
+    public WebElement usernameInputBox;
+
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    public WebElement passwordInputBox;
+
+    @FindBy(xpath = "//input[@value='Log In']")
+    public WebElement loginButton;
+
+    @FindBy(id = "LIVEFEED_search")
+    public WebElement FilterAndSearchInputBox;
+
+    @FindBy(xpath = "//span[@class='main-ui-filter-sidebar-item-text']")
+    public List<WebElement> listInTheFilterAndSearch;
+
+    @FindBy(xpath = "//span[@class='main-ui-filter-field-add-item']")
+    public WebElement addFieldButton;
+
 }
--- src/test/java/com/blueCRM/step_definitions/Step_Def_mehmet.java
+++ src/test/java/com/blueCRM/step_definitions/Step_Def_mehmet.java
@@ -1,4 +1,63 @@
 package com.blueCRM.step_definitions;
 
+import com.blueCRM.pages.BasePage;
+import com.blueCRM.pages.Pages_mehmet;
+import com.blueCRM.utilities.BrowserUtils;
+import com.blueCRM.utilities.ConfigurationReader;
+import com.blueCRM.utilities.Driver;
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Step_Def_mehmet {
+    Pages_mehmet pages_mehmet = new Pages_mehmet();
+    Faker faker = new Faker();
+
+    @Given("User is on the blueCRM login page")
+    public void user_is_on_the_blueCRM_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        pages_mehmet.usernameInputBox.click();
+        String email = "hr"+faker.numerify("6#")+"@cybertekschool.com";
+        pages_mehmet.usernameInputBox.sendKeys(email);
+        BrowserUtils.sleep(1);
+        pages_mehmet.passwordInputBox.click();
+        pages_mehmet.passwordInputBox.sendKeys(ConfigurationReader.getProperty("password"));
+        pages_mehmet.loginButton.click();
+        BrowserUtils.sleep(1);
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), ConfigurationReader.getProperty("mehmetVerifyUrl"));
+        System.out.println("Driver.getDriver().getCurrentUrl() = " + Driver.getDriver().getCurrentUrl());
+
+    }
+
+    // AC : 1
+    @When("Users click on filter and search input box")
+    public void users_click_on_filter_and_search_input_box() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(pages_mehmet.FilterAndSearchInputBox));
+        pages_mehmet.FilterAndSearchInputBox.click();
+    }
+    @Then("Verify filters and search box should be contain the following in order as default")
+    public void verify_filters_and_search_box_should_be_contain_the_following_in_order_as_default(List<String> expectedFilters) {
+        List<WebElement> listedElements = pages_mehmet.listInTheFilterAndSearch;
+        List<String> textOfListedElements = new ArrayList<>();
+        BrowserUtils.sleep(1);
+        for (WebElement each : listedElements) {
+            textOfListedElements.add(each.getText());
+        }
+        System.out.println(textOfListedElements);
+        Assert.assertTrue(textOfListedElements.containsAll(expectedFilters));
+    }
+
+    // AC : 2
+
+
 }
