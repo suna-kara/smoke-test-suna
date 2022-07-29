1a644865514e3d065c4bc02628cb5392c495835e
Enes <enstrkmn@hotmail.com>
2022-07-30 01:24:03 +0300

    updated

---
+++ src/test/java/com/blueCRM/step_definitions/step_def_Enes.java
@@ -0,0 +1,122 @@
+package com.blueCRM.step_definitions;
+
+import com.blueCRM.pages.Pages_Enes;
+import com.blueCRM.utilities.BrowserUtils;
+import com.blueCRM.utilities.ConfigurationReader;
+import com.blueCRM.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.*;
+
+public class step_def_Enes {
+
+    Pages_Enes pages_enes = new Pages_Enes();
+
+
+    @Given("user is on the library login page")
+    public void user_is_on_the_library_login_page() {
+        Driver.getDriver().get("https://qa.azulcrm.com/");
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+        BrowserUtils.sleep(1);
+
+        pages_enes.usernamebox.sendKeys(ConfigurationReader.getProperty("username"));
+
+        BrowserUtils.sleep(1);
+        pages_enes.passwordbox.sendKeys(ConfigurationReader.getProperty("password"));
+        BrowserUtils.sleep(1);
+        pages_enes.loginbutton.click();
+
+
+
+
+    }
+    @When("user clicks on the message tab")
+    public void user_clicks_on_the_message_tab() {
+
+        BrowserUtils.sleep(2);
+        pages_enes.messageTab.click();
+
+    }
+    @When("user clicks on the topic button")
+    public void user_clicks_on_the_topic_button() {
+
+        BrowserUtils.sleep(2);
+        pages_enes.TopicButton.click();
+
+
+    }
+    @When("user writes their topic name in the Topic")
+    public void user_writes_their_topic_name_in_the_topic() {
+
+        BrowserUtils.sleep(2);
+        pages_enes.TopicBox.sendKeys("Message Title");
+
+    }
+    @When("user writes their message in the message field")
+    public void user_writes_their_message_in_the_message_field() {
+
+
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
+
+        BrowserUtils.sleep(2);
+
+        pages_enes.textBox.sendKeys("User wrote a message");
+
+        Driver.getDriver().switchTo().parentFrame();
+
+
+    }
+    @Then("Verify that users send a message by fiiling in the mandatory fields")
+    public void verify_that_users_send_a_message_by_fiiling_in_the_mandatory_fields() {
+
+
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(pages_enes.addMoreButton));
+        pages_enes.addMoreButton.click();
+
+        BrowserUtils.sleep(2);
+
+        pages_enes.employessanddepartmentsButton.click();
+
+        BrowserUtils.sleep(2);
+
+        pages_enes.departmentEmployees.click();
+
+        BrowserUtils.sleep(2);
+
+        pages_enes.departmentEmployees2.click();
+
+        BrowserUtils.sleep(2);
+
+        assertTrue(pages_enes.selectedEmployess1.isDisplayed());
+        assertTrue(pages_enes.selectedEmployess2.isDisplayed());
+
+        BrowserUtils.sleep(2);
+
+        pages_enes.deletesymbol1.click();
+
+        BrowserUtils.sleep(3);
+
+
+
+        pages_enes.deletesymbol1.click();
+
+        BrowserUtils.sleep(2);
+
+
+
+
+    }
+
+
+}
--- src/test/resources/features/enes.feature
+++ src/test/resources/features/enes.feature
@@ -1,4 +1,4 @@
-@wip
+
 Feature:send message
   User Story:
   US01_As a user, I should be able to send messages by clicking on Message tab under Active Stream
@@ -7,10 +7,19 @@ Feature:send message
   Account is mail:helpdesk1@cybertekschool.com
   password:UserUser
 
-  Background:User should login with correct credential
+
+
+    Background:User should login with correct credential
+    Given user is on the library login page
+
+
+  @wipEnes
+  Scenario:Verify that users send a message by fiiling in the mandatory fields.
+    When user clicks on the message tab
+    And user clicks on the topic button
+    And user writes their topic name in the Topic
+    And user writes their message in the message field
+    Then Verify that users send a message by fiiling in the mandatory fields
 
 
 
-  Given user is on the library login page
-
-  Scenario: asdasdasd
